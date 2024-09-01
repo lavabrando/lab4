1a9bda6846d245642abe76359574c5aa7ba1bc7d
lavabrando <1359241601@qq.com>
2024-09-02 00:21:05 +1000

    100% complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -6,16 +6,30 @@ public class PrintAndHide : MonoBehaviour
 {
     int i = 3;
     public Renderer rend;
+    int RandomNumber;
     // Start is called before the first frame update
     void Start()
     {
         i++;
+        RandomNumber = Random.Range(150, 250);
     }
 
     // Update is called once per frame
     void Update()
     {   
         Debug.Log(gameObject.name+":"+i);
-        i++; 
+        i++;
+        if (i == 100) {
+            if (gameObject.CompareTag("Red"))
+            {
+                gameObject.SetActive(false);
+            }
+        }
+        if (i == RandomNumber) {
+            if (gameObject.CompareTag("Blue"))
+            {
+                gameObject.SetActive(false);
+            }
+        }
     }
 }
